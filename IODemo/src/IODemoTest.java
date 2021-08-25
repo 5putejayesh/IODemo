58d3bb45fe809c3d2f4f37dcbec344de09fb6884
PachputeJayesh <PachputeJayesh@LAPTOP-2RI2BBN0>
2021-08-25 13:12:42 +0530

    Sharing.

---
+++ IODemo/src/IODemoTest.java
@@ -0,0 +1,30 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class IODemoTest {
+
+	public static void main(String[] args) {
+		try {
+			FileOutputStream fos= new FileOutputStream("fos.txt");
+			fos.write((int)123);
+			fos.close();
+		}  catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			FileInputStream fis= new FileInputStream("fos.txt");
+			System.out.println((char)fis.read());
+			fis.close();
+		}  catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
